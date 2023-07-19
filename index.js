39c170d10c7aadf4133fd5a50e3ba398575331ca
Deep Roy <121348774+Deeproy007311@users.noreply.github.com>
2023-07-20 02:25:09 +0530

    files are added

---
+++ index.js
@@ -0,0 +1,77 @@
+var buttons = document.querySelectorAll("button");
+for (let i of buttons) {
+    i.addEventListener("click", function() {
+       var buttonInnerHtml = this.innerHTML;
+       console.log(buttonInnerHtml);
+       switch (buttonInnerHtml) {
+        case "w":
+            var tom1 = new Audio("tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            var tom2 = new Audio("tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            var tom3 = new Audio("tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            var tom4 = new Audio("tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            var kick = new Audio("kick-bass.mp3");
+            kick.play();
+            break;
+        case "k":
+            var snare = new Audio("snare.mp3");
+            snare.play();
+            break;
+        case "l":
+            var crash = new Audio("crash.mp3");
+            crash.play();
+            break;
+       
+        default:
+            console.log(buttonInnerHtml);
+       }
+    }); 
+}
+
+document.addEventListener("keypress",function(event) {
+    var key = event.key;
+    switch (key) {
+        case "w":
+            var tom1 = new Audio("tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            var tom2 = new Audio("tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            var tom3 = new Audio("tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            var tom4 = new Audio("tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            var kick = new Audio("kick-bass.mp3");
+            kick.play();
+            break;
+        case "k":
+            var snare = new Audio("snare.mp3");
+            snare.play();
+            break;
+        case "l":
+            var crash = new Audio("crash.mp3");
+            crash.play();
+            break;
+       
+        default:
+            console.log(key);
+       }
+}) 
